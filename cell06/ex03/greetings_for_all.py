023f45cb22fe6f96f1f65b796fd9af448e7e84e5
pckzy <141670672+pckzy@users.noreply.github.com>
2025-09-15 20:27:59 +0700

    Add files via upload

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,13 @@
+def greetings(name=None):
+    if (name is None or name == ''):
+        print("Hello, noble stranger.")
+    elif (isinstance(name, str)):
+        print(f'Hello, {name}.')
+    else:
+        print('Error! It was not a name.')
+
+
+greetings('Alexandra')
+greetings('Wil')
+greetings()
+greetings(42)
