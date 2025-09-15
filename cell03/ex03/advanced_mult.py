faa252b23b2eb6558601d88a1812beedd037c2c6
pckzy <141670672+pckzy@users.noreply.github.com>
2025-09-16 01:42:37 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,9 +1,14 @@
-i = 0
-while (i <= 10):
-    count = 0
-    print(f'Table de {i}:', end=" ")
-    while (count <= 10):
-        print(count*i, end=" ")
-        count += 1
-    i += 1
-    print()
+import sys
+
+if len(sys.argv) == 1:
+    i = 0
+    while (i <= 10):
+        count = 0
+        print(f'Table de {i}:', end=" ")
+        while (count <= 10):
+            print(count*i, end=" ")
+            count += 1
+        i += 1
+        print()
+else:
+    print('none')
