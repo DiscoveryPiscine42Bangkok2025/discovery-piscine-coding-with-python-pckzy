d13b7d309ece8e140898b5e9e0ff3b84c6d66191
pckzy <141670672+pckzy@users.noreply.github.com>
2025-09-15 16:18:31 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+first_num = int(input('Enter the first number:\n'))
+second_num = int(input('Enter the second number:\n'))
+
+mult = first_num * second_num
+
+print(f'{first_num} x {second_num} = {mult}')
+
+if (mult > 0):
+    print('The result is positive.')
+elif (mult < 0):
+    print('The result is negative.')
+else:
+    print('The result is positive and negative.')
